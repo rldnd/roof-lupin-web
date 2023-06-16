dfd21aeba37559b4d0401e0c2526e18ba4f0c912
rldnd <gi981226@gmail.com>
2023-06-16 15:18:53 +0900

    feat(component): button

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -4,11 +4,20 @@ import cx from "clsx";
 
 import styles from "./button.module.scss";
 
-interface Props extends ComponentProps<"button"> {}
+type Color = "primary" | "secondary";
 
-const Button: React.FC<Props> = ({ className, type, children, ...props }) => {
+interface Props extends ComponentProps<"button"> {
+  color: Color;
+  full?: boolean;
+}
+
+const Button: React.FC<Props> = ({ className, type, children, color, full = false, ...props }) => {
   return (
-    <button type={type ?? "button"} className={cx(styles.wrapper, className)} {...props}>
+    <button
+      type={type ?? "button"}
+      className={cx(styles.wrapper, styles[color], className, { [styles.full]: full })}
+      {...props}
+    >
       {children}
     </button>
   );
