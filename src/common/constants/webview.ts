2770dbb858b0ba4da4af29606127dba4ecb59d98
rldnd <gi981226@gmail.com>
2023-07-31 18:35:58 +0900

    fix(user-agent): ios userAgent

--- src/common/constants/webview.ts
+++ src/common/constants/webview.ts
@@ -1,4 +1,4 @@
 export const APP_USER_AGENT = "RoofLupinApplication";
 
 export const APP_USER_AGENT_ANDROID = "RoofLupinApplication-android";
-export const APP_USER_AGENT_IOS = "RoofLupinApplication-ios";
+export const APP_USER_AGENT_IOS = "RoofLupinApplication-iphone-ios";
