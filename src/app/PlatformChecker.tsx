95dc45d2c7f2e4dae4d74ae58641b0549f5f607c
rldnd <gi981226@gmail.com>
2023-08-01 13:30:54 +0900

    feat(platform): check platform with event (starts with sendMessage)

--- src/app/PlatformChecker.tsx
+++ src/app/PlatformChecker.tsx
@@ -2,15 +2,16 @@
 
 import { useSetAtom } from "jotai";
 
-import type { AppCommonPlatformPayload } from "@/common/types/webview/common";
+import type { AppCommonPlatformPayload, WebCommonPlatformPayload } from "@/common/types/webview/common";
 import { useClientEffect, useWebview } from "@/hooks";
 import { platformState } from "@/states/platform";
 
 const PlatformChecker: React.FC = () => {
   const setPlatform = useSetAtom(platformState);
-  const { addListener, removeListener } = useWebview();
+  const { addListener, sendMessage, removeListener } = useWebview();
 
   useClientEffect(() => {
+    sendMessage<WebCommonPlatformPayload>({ type: "web-common/platform" });
     addListener<AppCommonPlatformPayload>("app-common/platform", ({ platform }) => {
       setPlatform(platform);
     });
--- src/common/types/webview/common.d.ts
+++ src/common/types/webview/common.d.ts
@@ -1,10 +1,13 @@
-import type { GenerateWebviewPayload } from ".";
+import type { GenerateWebviewPayload, WithoutData } from ".";
 
+type BaseWebCommonPayload<T extends string, U = unknown> = GenerateWebviewPayload<"web", "common", T, U>;
 type BaseAppCommonPayload<T extends string, U = unknown> = GenerateWebviewPayload<"app", "common", T, U>;
 
 export interface AppCommonPlatformData {
   platform: "ios" | "android";
 }
 
+/** 플랫폼 확인 요청 */
+export type WebCommonPlatformPayload = BaseWebCommonPayload<"platform", WithoutData>;
 /** 웹뷰 플랫폼 체크 */
 export type AppCommonPlatformPayload = BaseAppCommonPayload<"platform", AppCommonPlatformData>;
