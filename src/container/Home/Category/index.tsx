13ecf76526a0297b08ea0c089947544a9d1c0680
rldnd <gi981226@gmail.com>
2023-08-02 18:32:08 +0900

    feat(home): category ui

--- src/container/Home/Category/index.tsx
+++ src/container/Home/Category/index.tsx
@@ -13,7 +13,9 @@ const Category: React.FC<Props> = ({ categories }) => {
     <nav className={styles.wrapper}>
       {categories.map((category) => (
         <Link key={category.id} className={styles.item} href={`/categories?categoryId=${category.id}`}>
-          <img src={category.iconPath} alt={`${category.name} 카테고리 이미지`} />
+          <div className={styles.imageWrapper}>
+            <img src={category.iconPath} alt={`${category.name} 카테고리 이미지`} />
+          </div>
           <span className={styles.name}>{category.name}</span>
           {category.isRecommended && <span className={styles.recommend}>추천</span>}
         </Link>
