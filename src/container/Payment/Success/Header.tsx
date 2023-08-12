0a427394d1a5baa3e7a38b196860eba1b0c56ae4
rldnd <gi981226@gmail.com>
2023-08-12 18:34:48 +0900

    feat(payment-success): closeButton

--- src/container/Payment/Success/Header.tsx
+++ src/container/Payment/Success/Header.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import { IconClose } from "public/icons";
 
 import styles from "./header.module.scss";
 
 const Header: React.FC = () => {
+  const { replace } = useRouter();
+
   return (
     <header className={styles.wrapper}>
-      <button type="button" className={styles.closeButton}>
+      <button type="button" className={styles.closeButton} onClick={() => replace("/")}>
         <IconClose />
       </button>
       <p className={styles.title}>결제 완료</p>
