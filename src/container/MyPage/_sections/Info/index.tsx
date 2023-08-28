954c713a540597ad4a3dce36b6faec773718e511
rldnd <gi981226@gmail.com>
2023-08-28 14:14:20 +0900

    feat(my-page): [WIP] top section

--- src/container/MyPage/_sections/Info/index.tsx
+++ src/container/MyPage/_sections/Info/index.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import type { CommonUser } from "@/common/types/user";
-import { useSuspenseQuery } from "@/hooks";
-import { getMeApi } from "@/services/user";
+import Image from "next/image";
+
+import { useMe } from "@/hooks/queries";
+
+import { IconGrayRightChevron } from "public/icons";
 
 import styles from "./info.module.scss";
 
 const Info: React.FC = () => {
-  const { data: me } = useSuspenseQuery<CommonUser>(["getMe"], getMeApi, { refetchOnMount: true });
+  const { me } = useMe();
 
   return (
     <section className={styles.wrapper}>
@@ -17,6 +19,13 @@ const Info: React.FC = () => {
           <br />
           안녕하세요.
         </h1>
+        {me?.profileImage && (
+          <Image className={styles.thumbnail} src={me.profileImage} width={60} height={60} alt="프로필 이미지" />
+        )}
+        {!me?.profileImage && <div className={styles.emptyImage} />}
+        <span className={styles.email}>
+          {me?.email} <IconGrayRightChevron />
+        </span>
       </div>
     </section>
   );
