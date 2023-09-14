1a09d84c2984bcde50617b0cc68cf20db654b8ca
rldnd <gi981226@gmail.com>
2023-09-14 20:07:53 +0900

    feat(my-profile-birth): api connect

--- src/components/BottomSheets/MyPage/Birth/index.tsx
+++ src/components/BottomSheets/MyPage/Birth/index.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import { ChangeEventHandler, FormEventHandler, useEffect, useState } from "react";
 
 import { useMutation } from "@tanstack/react-query";
+import { useUnmount } from "react-use";
 
 import type { CommonUser } from "@/common/types/user";
 import { BaseBottomSheet, Button, UnderlinedInput } from "@/components/Common";
+import { useToast } from "@/hooks";
 import { useMe } from "@/hooks/queries";
+import { getErrorMessage } from "@/services/apiClient";
 import { updateMeApi } from "@/services/user";
 import { checkValidDate, dayjs } from "@/utils/date";
 import { isNumberOnly } from "@/utils/regex";
@@ -53,11 +56,19 @@ const getInputStatus = (value: string): InputStatus => {
   return { isValid: true, errorMessage: "" };
 };
 
-// TODO: 로직 입력
 const MyBirthBottomSheet: React.FC<Props> = ({ isShow, onClose }) => {
+  const { addToast } = useToast();
   const { me, refetchMe } = useMe();
-  const { mutate } = useMutation(updateMeApi);
-  const [input, setInput] = useState(() => getInitialInput(me!));
+  const { mutate } = useMutation(updateMeApi, {
+    onSuccess: () => {
+      refetchMe();
+      onClose();
+    },
+    onError: (err) => {
+      addToast({ message: getErrorMessage(err, "생년월일 변경에 실패하였습니다.") });
+    },
+  });
+  const [input, setInput] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -70,7 +81,15 @@ const MyBirthBottomSheet: React.FC<Props> = ({ isShow, onClose }) => {
 
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    const birthYear = input.slice(0, 4);
+    const birthDay = input.slice(4, 8);
+    mutate({ birthYear, birthDay });
   };
+
+  useEffect(() => {
+    if (isShow && me) setInput(getInitialInput(me));
+    if (!isShow) setInput("");
+  }, [isShow, me]);
 
   if (!me) return null;
 
