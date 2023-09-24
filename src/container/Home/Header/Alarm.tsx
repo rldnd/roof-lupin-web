2ade929339b89091f0f6457f045e4be4ed8eae4a
rldnd <gi981226@gmail.com>
2023-09-25 01:26:07 +0900

    fix(home): alarm authChecker

--- src/container/Home/Header/Alarm.tsx
+++ src/container/Home/Header/Alarm.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import cx from "clsx";
 
 import type { UnReadAlarm } from "@/common/types/alarm";
+import { AuthChecker } from "@/components";
 import { useSuspenseQuery } from "@/hooks";
 import { getUnReadAlarm } from "@/services/alarm";
 
@@ -17,19 +19,22 @@ interface Props {
 }
 
 const Alarm: React.FC<Props> = ({ className }) => {
+  const { push } = useRouter();
   const { data } = useSuspenseQuery<UnReadAlarm>(["getUnReadAlarm"], getUnReadAlarm, {
     refetchOnMount: true,
     useErrorBoundary: false,
   });
 
   return (
-    <Link
-      href="/alarms"
-      title="알림 페이지"
-      className={cx(styles.wrapper, className, { [styles.isUnread]: data && data.isExists })}
-    >
-      <IconBell />
-    </Link>
+    <AuthChecker>
+      <button
+        onClick={() => push("/alarms")}
+        title="알림 페이지"
+        className={cx(styles.button, className, { [styles.isUnread]: data && data.isExists })}
+      >
+        <IconBell />
+      </button>
+    </AuthChecker>
   );
 };
 
