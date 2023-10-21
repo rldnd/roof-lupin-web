c8e2bd45d5b2eeabd1421b35d9e4aa2311db96b2
rldnd <gi981226@gmail.com>
2023-10-22 00:12:10 +0900

    feat(footer): [WIP] policies

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -35,10 +35,19 @@ const Footer: React.FC<Props> = ({ className }) => {
           대표: 강동현
         </p>
         <p>사업자등록번호 : 383-26-01563</p>
+        <p>통신판매업 : 2023-서울광진-1650</p>
         <p>주소 : 서울특별시 광진구 광나루로 436, 1층 101호</p>
         <p>대표 이메일 : contact@cumuco.net</p>
         <p>대표 연락처 : 070-8028-3628</p>
       </address>
+      <nav className={styles.policies}>
+        <button type="button">이용약관</button>
+        <button type="button">개인정보처리방침</button>
+        <button type="button">운영정책</button>
+      </nav>
+      <p className={styles.bottomText}>
+        쿠무코는 통신판매중개자로서, 상품의 예약, 이용 및 환불 등과 관련한 의무와 책임은 각 판매자에게 있습니다.
+      </p>
     </footer>
   );
 };
