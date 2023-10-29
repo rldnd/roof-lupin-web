2662f53adc7d0b6d4f66386a2ae0c6bd9a7c029a
rldnd <gi981226@gmail.com>
2023-10-30 04:09:31 +0900

    fix(qa-19): metadata

--- src/app/spaces/[spaceId]/reviews/page.tsx
+++ src/app/spaces/[spaceId]/reviews/page.tsx
@@ -1,1 +1,27 @@
+import type { Metadata } from "next";
+
+import { getServerSpaceApi } from "@/services/space";
+
+interface Props {
+  params: {
+    spaceId: string;
+  };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const space = await getServerSpaceApi(params.spaceId);
+
+  return {
+    title: `${space.title}에 대한 리뷰`,
+    keywords: [
+      space.title,
+      ...space.categories.map((category) => category.name),
+      "루프루팡",
+      "루프탑",
+      "공간대여",
+      "옥상",
+    ],
+  };
+}
+
 export { default } from "@/container/Space/[SpaceId]/Review";
