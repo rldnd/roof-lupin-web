7d580929a0bf35f0be8b2cfd32aef7091c4377fb
rldnd <gi981226@gmail.com>
2023-11-15 15:54:48 +0900

    chore(build): vercel.sh

--- src/scripts/vercel.sh
+++ src/scripts/vercel.sh
@@ -2,8 +2,10 @@
  
 if [[ $VERCEL_GIT_COMMIT_REF == "main"  ]] ; then 
   echo "[main branch] - production"
+  yarn install
   yarn build:prod
 else
   echo "[develop branch] - staging"
+  yarn install
   yarn build:staging
 fi
