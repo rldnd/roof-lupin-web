6b73e58b5c068212a7c877177d1c3a43af28756d
kyoongdev <9898junjun2@gmail.com>
2023-11-18 18:15:32 +0900

    feat(service): service icon

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -2,5 +2,5 @@ import type { Service } from "@/common/types/service";
 
 export const getServiceCategoryIconPath = (service: Service) => {
   if (service.icons.length === 0) return "";
-  return service.icons[0].iconPath;
+  return service.icons.find((icon) => !icon.isSelected)?.iconPath;
 };
